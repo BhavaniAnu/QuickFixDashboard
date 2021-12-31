db13cd118ea21b9c8730ddd20accca8b62a31571
BhavaniAnu <anubhavani5@gmail.com>
2021-12-31 11:24:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/login/login.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { SidenavComponent } from './common/components/sidenav/sidenav.component';
+import { HeaderComponent } from './common/components/header/header.component';
+import { FooterComponent } from './common/components/footer/footer.component';
+import { OrdersComponent } from './modules/orders/orders.component';
+import { UsersComponent } from './modules/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SidenavComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrdersComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
